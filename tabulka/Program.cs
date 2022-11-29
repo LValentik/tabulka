9072eb614dbf5f7d0880bc4b10b1672c088d0b62
Valentík Lukáš <Valentik.Lu.2022@skola.ssps.cz>
2022-11-29 11:04:01 +0100

    v1

---
+++ tabulka/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace tabulka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vožte počet řádků: ");
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vložte počet buňek: ");
+            int b = int.Parse(Console.ReadLine());
+            int c = 0;
+            for (int i = 0; i < r; i++)
+            {
+                for (int f = 0; f < 3; f++)
+                {
+                    c++;
+                    for (int e = 0; e < b; e++)
+                    {
+                        Console.Write("+");
+                    }
+                    if (c != 3)
+                    {
+
+                       for (int g = 0; g < b; g++)
+                        {
+                            Console.Write("-");
+                        }
+                    }
+                     
+                    
+                }
+                c = 0;
+                Console.WriteLine("");
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
